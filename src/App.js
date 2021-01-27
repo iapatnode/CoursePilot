4a9cc958906a347a9e8686171b9dd0fed847a41d
dybasjacob <dybasjt17@gcc.edu>
2021-01-27 10:11:55 -0500

    Added Login and SignUp files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Containers/Login'
+import SignUp from './Containers/SignUp'
+import { Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={Login}/>
+      <Route exact path="/SignUp" component={SignUp}/>
     </div>
   );
 }
---
+++ src/Containers/SignUp.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+
+function SignUp() {
+    return(
+        <div id="main-content">
+            <head>
+            <meta charset="UTF-8"></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            <title>Welcome to Course Pilot</title>
+        </head>
+            <body>
+                <main id="main-content">
+                    <h1> Welcome to Course Pilot Sign Up! </h1>
+                    <form id="login-form">
+                        <input type="email" name="email" id="email-field" className="login-form-field" placeholder="Email"></input>
+                        <input type="password" name="password" id="password-field" className="login-form-field" placeholder="password"></input>
+                        <input type="submit" value="login" id="login-form-submit"></input>
+                    </form>
+                    <p> Already have an account? Click <Link to="/">Here</Link> to login. </p>
+                </main>
+            </body>
+        </div>
+    )
+}
+
+export default SignUp;
