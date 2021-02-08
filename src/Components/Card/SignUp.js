0f8697bf4f36c6777faaddff1ce39c864f94aaa7
dybasjacob <jacobdybas@gmail.com>
2021-02-08 17:42:47 -0500

    Removed excess code from SignUp.js

--- src/Components/Card/SignUp.js
+++ src/Components/Card/SignUp.js
@@ -83,39 +83,6 @@ export const SignUp = () => {
                     </Form>
                     <div class="col"></div>
                 </div>
-                {/*<form id="signup-form" method="post" action="/api/signup">
-                    <input type="email" name="email" id="email-field" className="signup-form-field" placeholder="Enter Email"></input>
-                    <input type="text" name="username" id="username-field" className="signup-form-field" placeholder="Enter Username"></input>
-                    <input type="password" name="password" id="password-field" className="signup-form-field" placeholder="Enter Password"></input>
-                    <input type="password" name="confirm-password" id="confirm-password-field" className="signup-form-field" placeholder="Confirm Password"></input>
-                    <label for="major"> Select Your Major(s) </label>
-                    <select name="major" id="major">
-                        {success.majors.map((value, index) => {
-                            return <option key={index} value={value}>{value}</option>
-                        })}
-                    </select>
-                    <label for="minor"> Select Your Minor(s) </label>
-                    <select name="minor" id="minor">
-                        {success.minors.map((value, index) => {
-                            return <option key={index} value={value}> {value} </option>
-                        })}
-                    </select>
-                    <label for="requirement-year"> Select Your Requirement Year </label>
-                    <select name="requirement-year" id="requirement-year">
-                        <option value="2021">2021</option>
-                        <option value="2020">2020</option>
-                        <option value="2019">2019</option>
-                        <option value="2018">2018</option>
-                    </select>
-                    <label for="graduation-year"> Select Your Graduation Year </label>
-                    <select name="graduation-year" id="graduation-year">
-                        <option value="2021">2021</option>
-                        <option value="2022">2022</option>
-                        <option value="2023">2023</option>
-                        <option value="2024">2024</option>
-                    </select>
-                    <input type="submit" value="Create Account" id="login-form-submit" className="signup-form-field"></input>
-                </form>*/}
                 <p> Already have an account? Click <Link to="/">Here</Link> to login. </p>
             </div>
             <AdminFooter/>
