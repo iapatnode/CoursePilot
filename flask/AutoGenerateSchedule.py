abd3e4c71cac846e1d83a28ffc573fd06d948f36
se-mendheim <50588704+se-mendheim@users.noreply.github.com>
2021-02-14 15:22:51 -0500

    Generate Schedule
    gerer

--- flask/AutoGenerateSchedule.py
+++ flask/AutoGenerateSchedule.py
@@ -1,6 +1,45 @@
 # Sam's algorithm
 
 
+class Student:
+    def __init__(self, name, studentID):
+        self.name = name
+        self.studentID = studentID
+
+class Course:
+    def __init__(self, courseCode, prerequisites, timesAvail, semesterAvail, creditHours):
+        self.courseCode = courseCode
+        self.prerequisites = prerequisites
+        self.timesAvail = timesAvail
+        self.semesterAvail = semesterAvail
+        self.creditHours = creditHours
+
+
+
+def AutoGenerateSchedule():
+    
+
+    CoursesTaken = []
+    CoursesRequired = []
+
+    course1 = Course("ACCT 201", [], [], "fall", 3)
+    course2 = Course("ACCT 202", [], [], "spring", 3)
+    course3 = Course("ACCT 301", [], [], "fall", 3)
+    course4 = Course("")
+    course5 =
+    course6 =
+    course7 =
+    course8 =
+    course9 =
+    course10 =
+    course11 =
+    course12 =
+    course13 =
+    course14 =
+    course15 =
+
+    CoursesTaken.append()
+    
 
 
 '''
