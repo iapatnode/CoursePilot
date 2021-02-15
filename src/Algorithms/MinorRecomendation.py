827534cec1a3f563e63d5bde5afbd6f7f3d8110e
nic2b <nic2bgrube@gmail.com>
2021-02-15 12:42:20 -0500

    Update MinorRecomendation.py

--- src/Algorithms/MinorRecomendation.py
+++ src/Algorithms/MinorRecomendation.py
@@ -4,6 +4,40 @@
 2nd Pass: Sort the list of minors based on the classes that the student has not yet taken but needs to take.
 """
 
+#courseList will be a list of possible courses
+#numHoursReequired will contain the number of hours that need to be taken from the courseList
+#if it is based on course level, then requiredCourseLevel will != -1 and will be the level necessary
+class courseRequirement:
+    def __init__(self, courseList, numHoursRequired, hoursRemaining, requiredCourseLevel, classesNotAllowed, requirementMet):
+        self.courseList = courseList
+        self.numHoursRequired = numHoursRequired
+        self.hoursRemaining = hoursRemaining
+        self.requiredCourseLevel = requiredCourseLevel
+        self.classesNotAllowed = classesNotAllowed
+        self.requirementMet = requirementMet
+    
+    def containsClass(classToCheck):
+        if (classToCheck.courseCode in classesNotAllowed):
+            return False
+        elif (classToCheck.courseCode in courseList):
+            if (not requirementMet):
+                hoursRemainingInRequirement -= classToCheck.hours
+                return True
+            else:
+                return False
+        
+
+#requiredCourses is a list of course requirements
+#hoursRemaining is the hours that a given user has remaining
+class minor:
+    def __init__(self, requiredCourses, hoursRemaining): 
+        self.requiredCourses = requiredCourses
+        self.hoursRemaining = hoursRemaining
+
+classesTaken = []
+remainingClassesInMajor = []
+allMinors = []
+
 #1st Pass
 """
 for each classTaken:
@@ -11,6 +45,10 @@ for each classTaken:
         if classTaken is in minor.requirements:
             minor.hoursNeeded -= classTaken.hours
 """
+for (classTaken: classesTaken):
+    for (minor: allMinors):
+        if (minor.requiredCourses.containsClass(classTaken.code)):
+            minor.hoursRemaining -= classTaken.hours
 
 #2nd Pass
 """
@@ -19,6 +57,10 @@ for each classNotTakenButInMajor:
         if classNotTakenButInMajor is in minor.requirements:
             minor.hoursNeeded -= classNotTakenButInMajor.hours
 """
+for (remainingClass: remainingClassesInMajor):
+    for (minor: allMinors):
+        if (minor.requiredCourses.containsClass(remainingClass.code)):
+            minor.hoursRemaining -= remainingClass.hours
 
 #Database calls -- TO DO: Move this code to better location
 #get all minors of a given requirement year
