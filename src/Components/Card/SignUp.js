f44760e2a7f3398ff3d24d745b20a6f276007440
dybasjacob <jacobdybas@gmail.com>
2021-02-16 08:56:18 -0500

    Updated signup to be a little prettier

--- src/Components/Card/SignUp.js
+++ src/Components/Card/SignUp.js
@@ -6,6 +6,8 @@ import AdminFooter from "../Footers/AdminFooter.js";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Select from 'react-select'
+import Logo from '../static/images/logo.jpg'
+import Image from 'react-bootstrap/Image'
 
 
 export const SignUp = () => {
@@ -97,69 +99,74 @@ export const SignUp = () => {
                 <title>Welcome to Course Pilot</title>
             </div>    
             <div id="main-content">
-                <h1> Sign Up for Course Pilot </h1>
                 <div className="container">
+                    <div className="row-fluid">
+                        <Image src={Logo} fluid id="logo_signup"/>
+                    </div>
+                    <h1 id="signup-header"> Sign Up for Course Pilot </h1>
                     <div className="row">
-                        <div className="col-md-4 col-md-offset-4"></div>
-                        <Form id="signup-form">
-                            <Form.Group>
-                                <Form.Label>Email Address</Form.Label>
-                                <Form.Control onChange={handleEmailChange} type="email" name="email" id="email-field" className="signup-form-field" placeholder="Enter Email"></Form.Control>
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control onChange={handlePasswordChange} type="password" name="password" id="password-field" className="signup-form-field" placeholder="Enter Password"></Form.Control>
-                                <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control onChange={handleConfirmChange} type="password" name="confirm-password" id="confirm-password-field" className="signup-form-field" placeholder="Confirm Password"></Form.Control>
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label className="signup-form-field">Select Your Major(s)</Form.Label>
-                                <Select 
-                                        value={majorOptions.filter(obj => majorSelectedValue.includes(obj.value))}
-                                        onChange={handleMajorChange}
-                                        options={majorOptions} 
-                                        isMulti
-                                        isClearable
-                                        className="signup-form-field"
-                                        name="major"
-                                        id="major"
-                                    />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label className="signup-form-field">Select Your Minor(s)</Form.Label>
+                        <div className="col"></div>
+                        <div className="col-lg-6">
+                            <Form id="signup-form">
+                                <Form.Group>
+                                    <Form.Label>Email Address</Form.Label>
+                                    <Form.Control onChange={handleEmailChange} type="email" name="email" id="email-field" className="signup-form-field" placeholder="Enter Email"></Form.Control>
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control onChange={handlePasswordChange} type="password" name="password" id="password-field" className="signup-form-field" placeholder="Enter Password"></Form.Control>
+                                    <Form.Label>Confirm Password</Form.Label>
+                                    <Form.Control onChange={handleConfirmChange} type="password" name="confirm-password" id="confirm-password-field" className="signup-form-field" placeholder="Confirm Password"></Form.Control>
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label className="signup-form-field">Select Your Major(s)</Form.Label>
                                     <Select 
-                                        value={minorOptions.filter(obj => minorSelectedValue.includes(obj.value))}
-                                        onChange={handleChange}
-                                        options={minorOptions} 
-                                        isMulti
-                                        isClearable
-                                        className="signup-form-field"
-                                        name="minor"
-                                        id="minor"
-                                    />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label className="signup-form-field">Select Your Requirement Year</Form.Label>
-                                <Form.Control onChange={handleRequirementChange} as="select" name="requirement-year" id="requirement-year" className="signup-form-field">
-                                    <option value="2020">2020</option>
-                                    <option value="2019">2019</option>
-                                    <option value="2018">2018</option>
-                                    <option value="2017">2017</option>
-                                </Form.Control>
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label className="signup-form-field">Select Your Graduation Year</Form.Label>
-                                <Form.Control onChange={handleGraduationChange} as="select" name="graduation-year" id="graduation-year" className="signup-form-field">
-                                    <option value="2021">2021</option>
-                                    <option value="2022">2022</option>
-                                    <option value="2023">2023</option>
-                                    <option value="2024">2024</option>
-                                </Form.Control>
-                            </Form.Group>
-                            <Button onClick={handleSubmit} variant="primary" type="submit" id="signup-form-submit" className="signup-form-field">
-                                Create Account
-                            </Button>
-                        </Form>
+                                            value={majorOptions.filter(obj => majorSelectedValue.includes(obj.value))}
+                                            onChange={handleMajorChange}
+                                            options={majorOptions} 
+                                            isMulti
+                                            isClearable
+                                            className="signup-form-field"
+                                            name="major"
+                                            id="major"
+                                        />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label className="signup-form-field">Select Your Minor(s)</Form.Label>
+                                        <Select 
+                                            value={minorOptions.filter(obj => minorSelectedValue.includes(obj.value))}
+                                            onChange={handleChange}
+                                            options={minorOptions} 
+                                            isMulti
+                                            isClearable
+                                            className="signup-form-field"
+                                            name="minor"
+                                            id="minor"
+                                        />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label className="signup-form-field">Select Your Requirement Year</Form.Label>
+                                    <Form.Control onChange={handleRequirementChange} as="select" name="requirement-year" id="requirement-year" className="signup-form-field">
+                                        <option value="2020">2020</option>
+                                        <option value="2019">2019</option>
+                                        <option value="2018">2018</option>
+                                        <option value="2017">2017</option>
+                                    </Form.Control>
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label className="signup-form-field">Select Your Graduation Year</Form.Label>
+                                    <Form.Control onChange={handleGraduationChange} as="select" name="graduation-year" id="graduation-year" className="signup-form-field">
+                                        <option value="2021">2021</option>
+                                        <option value="2022">2022</option>
+                                        <option value="2023">2023</option>
+                                        <option value="2024">2024</option>
+                                    </Form.Control>
+                                </Form.Group>
+                                <Button onClick={handleSubmit} variant="primary" type="submit" id="signup-form-submit" className="signup-form-field">
+                                    Create Account
+                                </Button>
+                            </Form>
+                        </div>
                         <div className="col"></div>
                     </div>
                 </div>
