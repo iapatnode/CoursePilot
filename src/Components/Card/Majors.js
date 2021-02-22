f8335b0e52c0951c364c76649381af5d36831e25
se-mendheim <50588704+se-mendheim@users.noreply.github.com>
2021-02-21 23:25:10 -0500

    Changes to the major page
    yeeto

--- src/Components/Card/Majors.js
+++ src/Components/Card/Majors.js
@@ -1,11 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
+import '../static/styles/Majors-Style.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
 export const Majors = () => {
+
+    const [isLoading, setLoading] = useState(true);
+    const isFirstRun = useRef(true);
+    const [success, setSuccess] = useState(null);
+    const majorCourses = []
+
+    useEffect(() => {
+        axios.get("/api/MajorPage").then(response => {
+            setSuccess(response.data);
+            
+            console.log(response.data)
+        })
+    }, []);
+
+    //console.log(success.data)
+    //success.major.foreach(element => majorCourses.push({"value": element, "label": element}))
+    
+
     return (
         <div id="main-content">
+            
             <Navbar bg="dark" variant="dark" expand="lg">
             <Navbar.Brand href="/home">Course Pilot</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -15,11 +35,27 @@ export const Majors = () => {
                 <Nav.Link href="/degree">Degree Report</Nav.Link> 
                 <Nav.Link href="/majors">Majors and Minors</Nav.Link> 
                 <Nav.Link href="/profile">Profile</Nav.Link> 
+                
         </Nav>
         </Navbar.Collapse>
         </Navbar>
+        <div class="req-content">
+            <div class="reqYear">
+                Requirement Year
+            </div>
+            <div class="tab">
+                <button>Majors</button>
+                <button>Minors</button>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+        
         </div>
     );
 }
 
+
 export default Majors
+
