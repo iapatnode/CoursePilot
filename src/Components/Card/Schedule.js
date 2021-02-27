0a9d6de44e4ae4e4ff4232181cc041e0c94f80e7
jram99 <ramseyerje17@gcc.edu>
2021-02-26 20:58:03 -0500

    added times in military time format to courses

--- src/Components/Card/Schedule.js
+++ src/Components/Card/Schedule.js
@@ -189,8 +189,10 @@ class Schedule extends Component {
             para.appendChild(tag);
             var node = document.createTextNode(element["course_name"] + " " + element["course_section"]);
             var course = document.createTextNode(element["course_code"] + "\n");
+            var time = document.createTextNode(" " + element["course_time"] + " - " + element["course_end"])
             tag.appendChild(course);
             tag.appendChild(node);
+            tag.appendChild(time);
             tag.setAttribute("id", element["course_name"] + "*" + element["course_section"] + "*" + element["course_time"] + "*" + element["course_end"] + "!" + element["days"]);
             var element = document.getElementById("courses");
             element.appendChild(para);
