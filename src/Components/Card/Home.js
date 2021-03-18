c52ae37e8412a33e47fd7f1a802c40725ecc72ee
dybasjacob <jacobdybas@gmail.com>
2021-03-18 13:11:17 -0400

    Home page styling

--- src/Components/Card/Home.js
+++ src/Components/Card/Home.js
@@ -101,27 +101,29 @@ export const Home = ()=> {
               </Nav>
               </Navbar.Collapse>
             </Navbar>
-            <h1 id="home-header"> Schedules </h1>
+            <div className="container-fluid" id="home-header">
+                <h1 id="home-header"> Schedules </h1>
+            </div>
             <div className="container" id="schedule-list-view">
                 <div className="row">
                     <div className="col-md-4" id="names">
-                        <h2> Name </h2>
-                        <ul>
+                        <h2 id="name-header"> Name </h2>
+                        <ol>
                             {success.map((value, index) => {
                                 return <li id="schedule-name" onClick={clickListener} key={index} value={value["scheduleName"]}>{value["scheduleName"]}<br></br></li>
                             })}
-                        </ul>
+                        </ol>
                     </div>
                     <div className="col-md-4" id="semesters">
-                        <h2> Semester </h2>
+                        <h2 id="semester-header"> Semester </h2>
                         <ul>
                             {success.map((value, index) => {
-                                return <li id="schedule-semester" key={index} value={value["scheduleSemester"]}>{value["scheduleSemester"]}</li>
+                                return <li id="schedule-semester" key={index} value={value["scheduleSemester"]}>{value["scheduleSemester"]}<br></br></li>
                             })}
                         </ul>
                     </div>
                     <div className="col-md-4" id="dates">
-                        <h2> Date Modified </h2> 
+                        <h2 id="date-header"> Date Modified </h2> 
                         <ul>
                             {success.map((value, index) => {
                                 return <li id="schedule-date" key={index} value={value["dateModified"]}>{value["dateModified"]}</li>
