6456fcc50a4e370f5c02248f84e4fbac8f61aebf
dybasjacob <jacobdybas@gmail.com>
2021-05-05 18:04:41 -0400

    Shot in the dark

--- flask/MinorRecomendation.py
+++ flask/MinorRecomendation.py
@@ -325,6 +325,8 @@ def getRequiredCourses(user_email):
         cursor.execute("select degreeId from StudentMajorMinor WHERE email = %s;", (user_email,))
 
         print(f"User email in minor recommendation: {user_email}")
+        if not cursor.fetchall()[0]:
+            cursor.execute("select degreeId from StudentMajorMinor WHERE email = %s;", (user_email,))
         degreeID = cursor.fetchall()[0]
         
 
